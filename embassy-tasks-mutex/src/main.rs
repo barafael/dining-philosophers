f654dc02b1f8096482256031491c906ea6cc86c5
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2023-04-07 00:46:16 +0200

    Unmacrofy.

--- embassy-tasks-mutex/src/main.rs
+++ embassy-tasks-mutex/src/main.rs
@@ -39,11 +39,11 @@ async fn main(spawner: Spawner) {
     let random = RNG.lock().await.next_u32();
     defmt::info!("Random number: {}", random);
 
-    let philo_1 = philosopher_1();
-    let philo_2 = philosopher_2();
-    let philo_3 = philosopher_3();
-    let philo_4 = philosopher_4();
-    let philo_5 = philosopher_5();
+    let philo_1 = philosopher(&FORK_1, &FORK_2, 1, TIMEOUT);
+    let philo_2 = philosopher(&FORK_2, &FORK_3, 2, TIMEOUT);
+    let philo_3 = philosopher(&FORK_3, &FORK_4, 3, TIMEOUT);
+    let philo_4 = philosopher(&FORK_4, &FORK_5, 4, TIMEOUT);
+    let philo_5 = philosopher(&FORK_5, &FORK_1, 5, TIMEOUT);
 
     spawner.spawn(philo_1).unwrap();
     spawner.spawn(philo_2).unwrap();
@@ -75,75 +75,49 @@ fn all_forks_locked() -> bool {
         && FORK_5.try_lock().is_err()
 }
 
-macro_rules! philosopher {
-    ( $f1:expr, $f2:expr, $n:expr, $timeout:expr) => {
-        let mut activity = Activity::default();
-        loop {
-            match activity {
-                Activity::Thinking => {
-                    let duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 2);
-                    defmt::println!("Philosopher {} is thinking.", $n);
-                    Timer::after(Duration::from_millis(duration as u64)).await;
-                    activity = Activity::Eating;
-                }
-                Activity::Eating => {
-                    defmt::println!("Philosopher {} is hungry!", $n);
-                    let eat_duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 2);
-                    let wait_duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 32);
-                    let f1 = $f1.lock().await;
-                    let f2 = if !$timeout {
-                        $f2.lock().await
-                    } else {
-                        let f2 = $f2.lock();
-                        let timeout = embassy_time::with_timeout(
-                            Duration::from_millis(wait_duration as u64),
-                            f2,
-                        );
-                        match timeout.await {
-                            Ok(f2) => f2,
-                            Err(_e) => {
-                                defmt::println!(
-                                    "Philosopher {} gives up, goes back to thinking.",
-                                    $n
-                                );
-                                activity = Activity::Thinking;
-                                continue;
-                            }
+#[embassy_executor::task(pool_size = 5)]
+async fn philosopher(
+    f1: &'static Mutex<ThreadModeRawMutex, ()>,
+    f2: &'static Mutex<ThreadModeRawMutex, ()>,
+    id: u32,
+    timeout: bool,
+) -> ! {
+    let mut activity = Activity::default();
+    loop {
+        match activity {
+            Activity::Thinking => {
+                let duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 2);
+                defmt::println!("Philosopher {} is thinking.", id);
+                Timer::after(Duration::from_millis(duration as u64)).await;
+                activity = Activity::Eating;
+            }
+            Activity::Eating => {
+                defmt::println!("Philosopher {} is hungry!", id);
+                let eat_duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 2);
+                let wait_duration = RNG.lock().await.next_u32() as u8 / (u8::MAX / 32);
+                let f1 = f1.lock().await;
+                let f2 = if !timeout {
+                    f2.lock().await
+                } else {
+                    let f2 = f2.lock();
+                    let timeout =
+                        embassy_time::with_timeout(Duration::from_millis(wait_duration as u64), f2);
+                    match timeout.await {
+                        Ok(f2) => f2,
+                        Err(_e) => {
+                            defmt::println!("Philosopher {} gives up, goes back to thinking.", id);
+                            activity = Activity::Thinking;
+                            continue;
                         }
-                    };
-                    RUNNING.store(true, Ordering::SeqCst);
-                    defmt::println!("Philosopher {} is eating!", $n);
-                    Timer::after(Duration::from_millis(eat_duration as u64)).await;
-                    drop(f1);
-                    drop(f2);
-                    activity = Activity::Thinking;
-                }
+                    }
+                };
+                RUNNING.store(true, Ordering::SeqCst);
+                defmt::println!("Philosopher {} is eating!", id);
+                Timer::after(Duration::from_millis(eat_duration as u64)).await;
+                drop(f1);
+                drop(f2);
+                activity = Activity::Thinking;
             }
         }
-    };
-}
-
-#[embassy_executor::task]
-async fn philosopher_1() {
-    philosopher!(FORK_1, FORK_2, 1, TIMEOUT);
-}
-
-#[embassy_executor::task]
-async fn philosopher_2() {
-    philosopher!(FORK_2, FORK_3, 2, TIMEOUT);
-}
-
-#[embassy_executor::task]
-async fn philosopher_3() {
-    philosopher!(FORK_3, FORK_4, 3, TIMEOUT);
-}
-
-#[embassy_executor::task]
-async fn philosopher_4() {
-    philosopher!(FORK_4, FORK_5, 4, TIMEOUT);
-}
-
-#[embassy_executor::task]
-async fn philosopher_5() {
-    philosopher!(FORK_5, FORK_1, 5, TIMEOUT);
+    }
 }
